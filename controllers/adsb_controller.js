e1f8c4b025af0767ae258dfebb0e1f19aa11ec45
Satchel Hamilton <47583278+satchmakua@users.noreply.github.com>
2023-10-31 12:13:00 -0700

    Comments
    Added comments for clarity on adsb and oauth controllers.

--- controllers/adsb_controller.js
+++ controllers/adsb_controller.js
@@ -1,23 +1,22 @@
+// Function to retrieve ADS-B messages
 const getAdsbMessages = (req, res) => {
+    // Return a JSON response indicating that this endpoint is for ADS-B messages
     res.status(200).json({"adsb": "messages"});
 }
 
+// Function to register an ADS-B client
 const registerAdsbClient = (req, res) => {
-    // Extract client details from request body
+    // Extract client details from the request body
     const { clientName, clientDetails } = req.body;
 
-    // Assume a dummy registration logic for demo
+    // For demonstration purposes, assume a dummy registration logic and log the client details
     console.log(`Client ${clientName} registered with details: ${JSON.stringify(clientDetails)}`);
 
-    // Send a response back
+    // Send a success response back to the client
     res.status(200).json({ status: 'Client registered successfully' });
 }
 
-module.exports = {
-    getAdsbMessages,
-    registerAdsbClient
-}
-
+// Export the functions for use in other parts of the code
 module.exports = {
     getAdsbMessages,
     registerAdsbClient
--- controllers/oauth_controller.js
+++ controllers/oauth_controller.js
@@ -1,20 +1,25 @@
+// Function for handling OAuth login
 const oauthLogin = (req, res) => {
-    console.log("OAuth login route hit"); // debugging
-    res.status(200).json({"oauth": "login"});
+    console.log("OAuth login route hit"); // Debugging: Log that the OAuth login route was accessed
+    res.status(200).json({"oauth": "login"}); // Return a JSON response indicating successful OAuth login
 }
 
+// Function for handling OAuth logout
 const oauthLogout = (req, res) => {
-    res.status(200).json({"oauth": "logged out"});
+    res.status(200).json({"oauth": "logged out"}); // Return a JSON response indicating successful OAuth logout
 }
 
+// Function for refreshing OAuth tokens
 const oauthRefresh = (req, res) => {
-    res.status(200).json({"oauth": "token refreshed"});
+    res.status(200).json({"oauth": "token refreshed"}); // Return a JSON response indicating successful token refresh
 }
 
+// Function for handling OAuth callback
 const oauthCallback = (req, res) => {
-    res.status(200).json({"oauth": "callback"});
+    res.status(200).json({"oauth": "callback"}); // Return a JSON response indicating an OAuth callback
 }
 
+// Export the functions for use in other parts of the code
 module.exports = {
     oauthLogin,
     oauthCallback,
