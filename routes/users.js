4bcf3cece5e022bc6061c7141006f8ba99fd0aa7
Joshua Mathwich <joshua.mathwich@sagetech.com>
2023-11-03 19:17:54 -0700

    Formatted to meet Sagetech coding standards

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const {
     postUsers,
@@ -17,41 +17,39 @@ const {
     getConnectUserDevices,
     putDisconnectUserDevices,
     getAdsbUserDevices,
-} = require('../controllers/users_controller');
+} = require('../controllers/users_controller')
 
-router.post('', postUsers);
+router.post('', postUsers)
 
 // router.get('?limit=<param>&start[< "l,g" + "e, ">]=<param>',
-// getUsers);
+// getUsers)
 // once query is implemented uncomment line 24 and delete below line
-router.get('', getUsers);
+router.get('', getUsers)
 
-router.get('/:id', getID);
+router.get('/:id', getID)
 
-router.delete('/:id', deleteID);
+router.delete('/:id', deleteID)
 
-router.put('/:id', putID);
+router.put('/:id', putID)
 
-router.get('/:id/client/connect', getConnect);
+router.get('/:id/client/connect', getConnect)
 
-router.put('/:id/client/disconnect', putDisconnect);
+router.put('/:id/client/disconnect', putDisconnect)
 
-router.post('/:id/devices', postDevices);
+router.post('/:id/devices', postDevices)
 
-router.get('/:id/devices?limit=<param>&start[< "l,g" + "e, ">]=<param>', 
-getDevices);
+router.get('/:id/devices?limit=<param>&start[< "l,g" + "e, ">]=<param>', getDevices)
 
-router.get('/:id/devices/:id', getUserDevices);
+router.get('/:id/devices/:id', getUserDevices)
 
-router.delete('/:id/devices/:id', deleteUserDevices);
+router.delete('/:id/devices/:id', deleteUserDevices)
 
-router.put('/:id/devices/:id', putUserDevices);
+router.put('/:id/devices/:id', putUserDevices)
 
-router.get('/:id/devices/:id/connect', getConnectUserDevices);
+router.get('/:id/devices/:id/connect', getConnectUserDevices)
 
-router.put('/:id/devices/:id/disconnect', putDisconnectUserDevices);
+router.put('/:id/devices/:id/disconnect', putDisconnectUserDevices)
 
-router.get('/:id/devices/:id/adsb?start=<param>&end=<param> ',
-getAdsbUserDevices);
+router.get('/:id/devices/:id/adsb?start=<param>&end=<param> ', getAdsbUserDevices)
 
-module.exports = router;
+module.exports = router
