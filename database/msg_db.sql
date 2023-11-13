6e03392e8892090c5d563607e0ec9c4a5f270d45
Satchel Hamilton <47583278+satchmakua@users.noreply.github.com>
2023-11-13 14:25:09 -0800

    updated msg database schema

--- database/msg_db.sql
+++ database/msg_db.sql
@@ -1,8 +1,16 @@
+-- Message Database Schema
 CREATE TABLE adsb_messages (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(50),
-    raw_data BYTEA,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    message_id SERIAL PRIMARY KEY,
+    icao_address CHAR(6) NOT NULL,
+    call_sign VARCHAR(8),
+    altitude INTEGER,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    vertical_rate INTEGER,
+    ground_speed INTEGER,
+    track_angle INTEGER,
+    timestamp TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    message_data BYTEA NOT NULL
 );
 
 CREATE INDEX idx_adsb_messages_type ON adsb_messages(type);
