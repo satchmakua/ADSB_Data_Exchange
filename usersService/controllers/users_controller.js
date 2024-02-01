ae8471cd8c391002c43921ee802300730d65684f
aflores <alexander.flores@wsu.edu>
2024-02-01 15:54:46 -0800

    users_controller.js had conflicts
    I resolved it by copy/past3 main-dev users_controllers.js file because i originally branched from it.

--- usersService/controllers/users_controller.js
+++ usersService/controllers/users_controller.js
@@ -2,6 +2,7 @@ const Pool = require('pg').Pool
 const crypto = require('crypto')
 
 // TO-DO: set db up on WSU server and update these settings
+
 const pool = new Pool({
    user: "janna",
    host: "localhost",
@@ -9,49 +10,41 @@ const pool = new Pool({
    password: "password",
    port: 5432
 })
+// const path = require('path')
+// require('dotenv').config(
+//    {
+//       override: true,
+//       path: path.join(__dirname, '../dev.env')
+//    })
+// const pool = new Pool({
+//    user: process.env.USER,
+//    host: process.env.HOST,
+//    database: process.env.DATABASE,
+//    password: process.env.PASSWORD,
+//    port: process.env.DBPORT
+// });
 
 standardReturn = (res, error, results, errCode, customErrStr) =>
 {
-    if (error)
-    {
-        const code = errCode ?? 500
-        if (customErrStr)
-        {
-            res.status(code).send(customErrStr)
-        } else
-        {
-            res.status(code).send(`Error: ${error}`)
-        }
-        return
-    }
-    res.status(200).send(results.rows)
+   if (error)
+   {
+      const code = errCode ?? 500
+      if (customErrStr)
+      {
+         res.status(code).send(customErrStr)
+      } else
+      {
+         res.status(code).send(`Error: ${error}`)
+      }
+      return
+   }
+   res.status(200).send(results.rows)
 }
 
 // post '/users'
 // create user
 postUsers = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const { username, password } = req.body
-
-    const salt = crypto.randomBytes(32).toString('hex')
-    if (password == null || password.length < 1 || username == null || username.length < 1)
-    {
-        res.status(400).send('username or password not provided.')
-        return
-    }
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 32, 'sha256').toString("hex")
-
-    pool.query(`INSERT INTO users (username, password, salt) VALUES ('${username}', '${hash}', '${salt}') RETURNING *`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send('User registration failed.')
-            return
-        }
-        res.status(201).send(`User added with ID: ${results.rows[0].id}`)
-    })
-=======
    const { username, password } = req.body
 
    const salt = crypto.randomBytes(32).toString('hex')
@@ -71,45 +64,12 @@ postUsers = (req, res) =>
       }
       res.status(201).send(`User added with ID: ${results.rows[0].id}`)
    })
->>>>>>> Stashed changes:controllers/users_controller.js
 }
 
 // post '/users/validate'
 // validate that user login information is correct when user first logs into the application
 isValidUser = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const { username, password } = req.body;
-    let salt = ''
-
-    if (password == null || password.length < 1 || username == null || username.length < 1)
-    {
-        res.status(400).send('username or password not provided.')
-        return
-    }
-
-    pool.query(`SELECT salt from users WHERE username='${username}'`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send('User not found in the system.')
-            return
-        }
-        salt = results.rows[0].salt
-    })
-
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 32, 'sha256').toString("hex")
-
-    pool.query(`SELECT id FROM users WHERE username='${username}' AND password='${hash}'`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(400).send('Invalid Password.')
-            return
-        }
-        res.status(201).send('User Verified.')
-    })
-=======
    const { username, password } = req.body;
    let salt = ''
 
@@ -140,7 +100,6 @@ isValidUser = (req, res) =>
       }
       res.status(201).send('User Verified.')
    })
->>>>>>> Stashed changes:controllers/users_controller.js
 }
 
 
@@ -150,28 +109,16 @@ isValidUser = (req, res) =>
 // the below code will just return ALL users.
 getUsers = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send('Unable to retrieve user list.')
-            console.log("hit users")
-            return
-        }
-        res.status(200).json(results.rows)
-    })
-=======
    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) =>
    {
       if (error)
       {
          res.status(500).send('Unable to retrieve user list.')
+         console.log("hit users")
          return
       }
       res.status(200).json(results.rows)
    })
->>>>>>> Stashed changes:controllers/users_controller.js
 
 }
 
@@ -180,41 +127,16 @@ getUsers = (req, res) =>
 // Q: what should be returned? just username?
 getID = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    pool.query(`SELECT * FROM users WHERE id = ${id}`, (error, results) => standardReturn(res, error, results))
-=======
    const id = parseInt(req.params.id)
-   pool.query(`SELECT * FROM users WHERE id = ${id}`, (error, results) =>
-   {
-      if (error)
-      {
-         throw error
-      }
-      res.status(200).json(results.rows)
-   })
-
->>>>>>> Stashed changes:controllers/users_controller.js
+   pool.query(`SELECT * FROM users WHERE id = ${id}`, (error, results) => standardReturn(res, error, results))
 }
 
 // delete '/users/:id'
 // delete user given user id
 deleteID = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    pool.query(`DELETE FROM users WHERE id = ${id}`, (error, results) => standardReturn(res, error, results))
-=======
    const id = parseInt(req.params.id)
-   pool.query(`DELETE FROM users WHERE id = ${id}`, (error, results) =>
-   {
-      if (error)
-      {
-         throw error
-      }
-      res.status(200).json(results.rows)
-   })
->>>>>>> Stashed changes:controllers/users_controller.js
+   pool.query(`DELETE FROM users WHERE id = ${id}`, (error, results) => standardReturn(res, error, results))
 }
 
 // put '/users/:id'
@@ -222,54 +144,46 @@ deleteID = (req, res) =>
 // Q: just updating password? Option to update username or both?
 putID = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    // TO-DO
-    // const id = parseInt(req.params.id)
+   // TO-DO
+   // const id = parseInt(req.params.id)
 
-    // const { username, password } = req.body
+   // const { username, password } = req.body
 
-    // if (username && password) {
+   // if (username && password) {
 
-    // } else if (username) {
+   // } else if (username) {
 
-    // } else if (password) {
+   // } else if (password) {
 
-    // } else {
+   // } else {
 
-    // }
-    // const salt = crypto.randomBytes(32).toString('hex')
-    // if (password == null || password.length < 1 || username == null || username.length < 1 ) {
-    //     res.status(400).send('username or password not provided.')
-    //     return
-    // }
-    // const hash = crypto.pbkdf2Sync(password, salt, 1000, 32, 'sha256').toString("hex")
+   // }
+   // const salt = crypto.randomBytes(32).toString('hex')
+   // if (password == null || password.length < 1 || username == null || username.length < 1 ) {
+   //     res.status(400).send('username or password not provided.')
+   //     return
+   // }
+   // const hash = crypto.pbkdf2Sync(password, salt, 1000, 32, 'sha256').toString("hex")
 
 
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
 }
 
 // post '/users/:id/devices'
 // input a new device tied to user id
 postDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    const { mac_address, latitude, longitude } = req.body;
-    // should get mac_address / identifier from somewhere else???
-    pool.query(`INSERT INTO groundstations (user_id, mac_address, latitude, longitude) VALUES (${id}, '${mac_address}', ${latitude}, ${longitude}) RETURNING *`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send(`Error: ${error}`)
-            return
-        }
-        res.status(201).send(`Device ${mac_address} added with id ${results.rows[0].id} and associated with user ${id}`)
-    })
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
+   const id = parseInt(req.params.id)
+   const { mac_address, latitude, longitude } = req.body;
+   // should get mac_address / identifier from somewhere else???
+   pool.query(`INSERT INTO groundstations (user_id, mac_address, latitude, longitude) VALUES (${id}, '${mac_address}', ${latitude}, ${longitude}) RETURNING *`, (error, results) =>
+   {
+      if (error)
+      {
+         res.status(500).send(`Error: ${error}`)
+         return
+      }
+      res.status(201).send(`Device ${mac_address} added with id ${results.rows[0].id} and associated with user ${id}`)
+   })
 }
 
 // get '/users/:id/devices?limit=<param>&start[< "l,g" + "e, ">]=<param>'
@@ -277,80 +191,64 @@ postDevices = (req, res) =>
 // Q: so this query will only be for a specific user?
 getDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    pool.query(`SELECT * FROM groundstations WHERE user_id = ${id}`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send(`Error: ${error}`)
-            return
-        }
-        res.status(200).send(results.rows)
-    })
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
+   const id = parseInt(req.params.id)
+   pool.query(`SELECT * FROM groundstations WHERE user_id = ${id}`, (error, results) =>
+   {
+      if (error)
+      {
+         res.status(500).send(`Error: ${error}`)
+         return
+      }
+      res.status(200).send(results.rows)
+   })
 }
 
 // get '/users/:id/devices/:deviceid'
 // get device information given a device ID and user ID
 getUserDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    const deviceid = parseInt(req.params.deviceid)
-    pool.query(`SELECT * FROM groundstations WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) =>
-    {
-        if (error)
-        {
-            res.status(500).send(`Error: ${error}`)
-            return
-        }
-        res.status(200).send(results.rows)
-    })
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
+   const id = parseInt(req.params.id)
+   const deviceid = parseInt(req.params.deviceid)
+   pool.query(`SELECT * FROM groundstations WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) =>
+   {
+      if (error)
+      {
+         res.status(500).send(`Error: ${error}`)
+         return
+      }
+      res.status(200).send(results.rows)
+   })
 }
 
 // delete '/users/:id/devices/:deviceid'
 // delete a device tied to a specific user
 deleteUserDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    const deviceid = parseInt(req.params.deviceid)
-    pool.query(`DELETE FROM groundstations WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
+   const id = parseInt(req.params.id)
+   const deviceid = parseInt(req.params.deviceid)
+   pool.query(`DELETE FROM groundstations WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
 }
 
 // put '/users/:id/devices/:deviceid'
 // update a device tied to a specific user
 putUserDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    const id = parseInt(req.params.id)
-    const deviceid = parseInt(req.params.deviceid)
-    const { latitude, longitude } = req.body
-    if (latitude && longitude)
-    {
-        pool.query(`UPDATE groundstations SET latitude = ${latitude}, longitude = ${longitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
-    } else if (latitude)
-    {
-        pool.query(`UPDATE groundstations SET latitude = ${latitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
-    } else if (longitude)
-    {
-        pool.query(`UPDATE groundstations SET longitude = ${longitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
-    } else
-    {
-        res.status(402).send('Include latitude or longitude in body to update')
-    }
-=======
-   res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
+   const id = parseInt(req.params.id)
+   const deviceid = parseInt(req.params.deviceid)
+   const { latitude, longitude } = req.body
+   if (latitude && longitude)
+   {
+      pool.query(`UPDATE groundstations SET latitude = ${latitude}, longitude = ${longitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
+   } else if (latitude)
+   {
+      pool.query(`UPDATE groundstations SET latitude = ${latitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
+   } else if (longitude)
+   {
+      pool.query(`UPDATE groundstations SET longitude = ${longitude} WHERE user_id = ${id} AND id = ${deviceid}`, (error, results) => standardReturn(res, error, results))
+   } else
+   {
+      res.status(402).send('Include latitude or longitude in body to update')
+   }
 }
 
 // get 'users/:id/client/connect'
@@ -371,16 +269,11 @@ putDisconnect = (req, res) =>
 // get adsb messages from a to b
 getAdsbUserDevices = (req, res) =>
 {
-<<<<<<< Updated upstream:usersService/controllers/users_controller.js
-    // verify that device is assigned to user
-    // any o-auth related stuff here?
-    // prompt broker to set-up message queue from time a to b for this device
-    // redirect to give client information for websocket connection
-    res.status(200).json({})
-=======
-   /* Check for ws, */
+   // verify that device is assigned to user
+   // any o-auth related stuff here?
+   // prompt broker to set-up message queue from time a to b for this device
+   // redirect to give client information for websocket connection
    res.status(200).json({})
->>>>>>> Stashed changes:controllers/users_controller.js
 }
 
 module.exports = {
