8e07417349b1556c8e8374106fef860e9d23cda8
Janna Tanninen <janna.tikka@wsu.edu>
2024-04-02 13:26:27 -0700

    updates to get userservice working on ec2 instance

--- usersService/database/db.js
+++ usersService/database/db.js
@@ -7,11 +7,11 @@ require('dotenv').config
    })
 
 const client = pgp({
-   user: process.env.USER,
-   host: process.env.HOST,
-   database: process.env.DB_USER,
-   password: process.env.PASSWORD_USER,
-   port: process.env.DBPORT
+   user: process.env.ADSDB_USER,
+   host: process.env.ADSDB_HOST,
+   database: process.env.ADSDB_DB,
+   password: process.env.ADSDB_PASSWORD,
+   port: process.env.ADSDB_PORT
 });
 
 module.exports = client
