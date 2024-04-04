3752b7bc1edddfcd559984839450817287cc6de8
19floresa <alexander.flores0920@gmail.com>
2024-04-04 13:33:17 -0700

    added coment for user db so i can make it work in my stuff

--- usersService/database/db.js
+++ usersService/database/db.js
@@ -12,6 +12,11 @@ const client = pgp({
    database: process.env.ADSDB_DB,
    password: process.env.ADSDB_PASSWORD,
    port: process.env.ADSDB_PORT
+   // user: process.env.USER,
+   // host: process.env.HOST,
+   // database: process.env.DB_USER,
+   // password: process.env.PASSWORD_USER,
+   // port: process.env.DBPORT
 });
 
 module.exports = client
