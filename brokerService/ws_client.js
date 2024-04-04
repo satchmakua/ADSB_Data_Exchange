a7672d36d98740cfdb32e26f4cbd9a4079d6ade8
Janna Tanninen <janna.tikka@wsu.edu>
2024-04-04 11:51:52 -0700

    update

--- brokerService/ws_client.js
+++ brokerService/ws_client.js
@@ -11,11 +11,11 @@ if (process.argv[2] != 'websocket' || !userId || !deviceId)
 
 async function getAccessCode() {
    try {
-       const response = await axios.post('http://localhost:3000/users/validate', {
-           username: 'username',
+       const response = await axios.post('http://35.87.79.158:80/users/validate', {
+           username: 'admin1',
            password: 'password'
        });
-       return response.data.auth_code; // auth_code is actually send in the header
+       return response.headers.auth_code;
    } catch (error) {
        console.error('Error fetching access code:', error.message);
        throw error;
@@ -26,7 +26,7 @@ async function getAccessCode() {
 // user needs to call tokens/refresh
 async function getAuthorizationToken(authCode) {
    try {
-       const response = await axios.post('http://localhost:3000/users/tokens/login', {
+       const response = await axios.post('http://35.87.79.158:80/users/tokens/login', {
          header:
          {
            "auth_code": authCode
@@ -36,52 +36,64 @@ async function getAuthorizationToken(authCode) {
            "scope":"admin"
          }
        });
-       return response.data; // change because these are pulled from the header. contains access_token, refresh_token
+       return (response.headers.access_token, response.headers.refresh_token);
    } catch (error) {
        console.error('Error fetching authorization token:', error.message);
        throw error;
    }
 }
-// below functions are to successfully login and get oAuth token.
-const auth_code = await getAccessCode();
-const tokens = await getAuthorizationToken(auth_code);
 
-const socket = new WebSocket(`ws://localhost:3003/${userId}`)
-
-socket.on('open', () => {
-   socket.send(JSON.stringify(
-      {
-         type: 'init',
-         userId: userId
-      }))
-
-   console.log('WebSocket connection opened.')
-})
-
-socket.on('message', (data) => {
-   console.log('Received message from server:', JSON.parse(data))
-})
-
-socket.on('close', () => {
-   console.log('WebSocket connection closed.')
-})
-
-socket.on('error', (error) => {
-   console.error('WebSocket error:', error.message)
-})
-
-setTimeout(async function streamData() { // wait for websocket connection to be set-up
+async function main() {
    try {
-      const response = await axios.post(`http://localhost:3000/users/${userId}/devices/${deviceId}/stream`, {
-         headers: {
-            auth_code: auth_code,
-            access_token: tokens.access_token,
-            refresh_token: tokens.refresh_token
-         }
-         })
-      console.log(`Received data: ${JSON.stringify(response.data)}`)
-  } catch (error) {
-      console.log(`Error ${error.code}: ${error.message}`)
-  }
-}, 2000);
+      // below functions are to successfully login and get oAuth token.
+      const auth_code = await getAccessCode()
+      const tokens = await getAuthorizationToken(auth_code)
+      const access_token = tokens[0]
+      const refresh_token = tokens[1]
+
+      const socket = new WebSocket(`http://35.87.79.158:80/${userId}`)
+
+      socket.on('open', () => {
+         socket.send(JSON.stringify(
+            {
+               type: 'init',
+               userId: userId
+            }))
+
+         console.log('WebSocket connection opened.')
+      })
+
+      socket.on('message', (data) => {
+         console.log('Received message from server:', JSON.parse(data))
+      })
+
+      socket.on('close', () => {
+         console.log('WebSocket connection closed.')
+      })
+
+      socket.on('error', (error) => {
+         console.error('WebSocket error:', error.message)
+      })
+
+      setTimeout(async function streamData() { // wait for websocket connection to be set-up
+         try {
+            const response = await axios.post(`http://35.87.79.158:80/users/${userId}/devices/${deviceId}/stream`, {
+               headers: {
+                  auth_code: auth_code,
+                  access_token: access_token,
+                  refresh_token: refresh_token
+               }
+               })
+            console.log(`Received data: ${JSON.stringify(response.data)}`)
+      } catch (error) {
+            console.log(`Error ${error.code}: ${error.message}`)
+      }
+      }, 2000);
+
+} catch (error) {
+   console.error('Error:', error);
+}
+}
+
+main();
 
