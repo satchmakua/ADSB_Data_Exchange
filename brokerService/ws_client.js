bf89e3d9124e8ea0c659c634cad3ad1bf8d06124
19floresa <alexander.flores0920@gmail.com>
2024-04-07 18:42:57 -0700

    fixed axios problems in ws_client. You should be able to get/send a token. Problem: socket

--- brokerService/ws_client.js
+++ brokerService/ws_client.js
@@ -11,11 +11,11 @@ if (process.argv[2] != 'websocket' || !userId || !deviceId)
 
 async function getAccessCode() {
    try {
-       const response = await axios.post('http://35.87.79.158:80/users/validate', {
+       const response = (await axios.post('http://localhost:3000/users/validate', {
            username: 'admin1',
            password: 'password'
-       });
-       return response.headers.auth_code;
+       })).headers;
+       return response.auth_code;
    } catch (error) {
        console.error('Error fetching access code:', error.message);
        throw error;
@@ -26,63 +26,70 @@ async function getAccessCode() {
 // user needs to call tokens/refresh
 async function getAuthorizationToken(authCode) {
    try {
-       const response = await axios.post('http://35.87.79.158:80/users/tokens/login', {
+       const response = (await axios.post('http://127.0.0.1:3000/users/tokens/login', 
+       {
          "scope": "admin"
-      }, {
-         headers:
+       },{
+          headers:
          {
            "auth_code": authCode
          }
-       });
-       return (response.headers.access_token, response.headers.refresh_token);
+      })).headers;
+
+       return {
+         access_token:response.access_token, 
+         refresh_token:response.refresh_token
+      };
    } catch (error) {
        console.error('Error fetching authorization token:', error.message);
        throw error;
    }
 }
 
+ const socket = new WebSocket(`http://localhost:3003/${userId}`)
+ socket.on('open', () => {
+   socket.send(JSON.stringify(
+      {
+         type: 'init',
+         userId: userId
+      }))
+
+   console.log('WebSocket connection opened.')
+})
+socket.on('message', (data) => {
+   console.log('Received message from server:', JSON.parse(data))
+})
+      
+socket.on('close', () => {
+   console.log('WebSocket connection closed.')
+})
+      
+socket.on('error', (error) => {
+   console.error('WebSocket error:', error.message)
+})
+
 async function main() {
    try {
       // below functions are to successfully login and get oAuth token.
       const auth_code = await getAccessCode()
-      const tokens = await getAuthorizationToken(auth_code)
-      const access_token = tokens[0]
-      const refresh_token = tokens[1]
-
-      const socket = new WebSocket(`http://35.87.79.158:80/${userId}`)
-
-      socket.on('open', () => {
-         socket.send(JSON.stringify(
-            {
-               type: 'init',
-               userId: userId
-            }))
-
-         console.log('WebSocket connection opened.')
-      })
-
-      socket.on('message', (data) => {
-         console.log('Received message from server:', JSON.parse(data))
-      })
-      
-      socket.on('close', () => {
-         console.log('WebSocket connection closed.')
-      })
-      
-      socket.on('error', (error) => {
-         console.error('WebSocket error:', error.message)
-      })
+      console.log("code:",auth_code)
+      const {access_token, refresh_token} = await getAuthorizationToken(auth_code)
+     
+      console.log("access:",access_token)
+      console.log("refresh:",refresh_token)
+     
 
       setTimeout(async function streamData() { // wait for websocket connection to be set-up
          try {
-            const response = await axios.post(`http://35.87.79.158:80/users/${userId}/devices/${deviceId}/stream`, {
+            const response = await axios.post(`http://localhost:3000/users/${userId}/devices/${deviceId}/stream`, {}, {
                headers: {
                   "access_token": access_token,
-                  "refresh_token": refresh_token
+                  "refresh_token":  refresh_token
                }
                })
             console.log(`Received data: ${JSON.stringify(response.data)}`)
       } catch (error) {
+         console.log(error)
             console.log(`Error ${error.code}: ${error.message}`)
       }
       }, 2000);
