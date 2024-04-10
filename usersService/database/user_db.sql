fa0962e227e6c6969f62676c9089c87f9cb9125b
Janna Tikka <janna.tikka@wsu.edu>
2024-04-10 17:20:57 +0000

    merge main-dev into main

---
+++ usersService/database/user_db.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS groundstations;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+	id serial PRIMARY KEY,
+ 	username text UNIQUE,
+	password text,
+	salt text
+) WITH (OIDS = FALSE);
+
+/*
+the relation between users and groundstations is one to many,
+i.e. a user can have many groundstations but each groundstation should only
+be tied to one user. So each ground station will only exist once in this table. 
+If this changes, we need to do a join table.
+*/
+CREATE TABLE groundstations
+(
+	id serial PRIMARY KEY,
+	user_id int,
+	mac_address text UNIQUE,
+	latitude int,
+	longitude int,
+   FOREIGN KEY(user_id) REFERENCES users(id)
+) WITH (OIDS = FALSE);
+)
---
+++ usersService/oauth/verify_route.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+   postLoginExchange,
+   postRefreshTokens,
+} = require('./verify_controllers')
+const {
+   request_auth_code_verification,
+   request_refresh_token_verification,
+} = require('./verify_tokens_middleware')
+
+
+
+
+/* request for the first access and refresh tokens */
+router.post('/users/tokens/login', request_auth_code_verification, postLoginExchange)
+/* request for tokens to be refreshed */
+router.post('/users/tokens/refresh', request_refresh_token_verification, postRefreshTokens)
+
+
+
+
+
+
+module.exports = router
