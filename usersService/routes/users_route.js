713f4610dbe5b1965f15ea384d7cce5741ce793c
Janna Tanninen <janna.tikka@wsu.edu>
2024-05-02 16:10:24 -0700

    fix

--- usersService/routes/users_route.js
+++ usersService/routes/users_route.js
@@ -6,8 +6,8 @@ const {
    getID,
    deleteID,
    putID,
-   getConnect,
-   putDisconnect,
+   //getConnect,
+   //putDisconnect,
    postDevices,
    getDevices,
    getUserDevices,
@@ -30,10 +30,6 @@ router.delete('/:id', deleteID)
 
 router.put('/:id', putID)
 
-router.get('/:id/client/connect', getConnect)
-
-router.put('/:id/client/disconnect', putDisconnect)
-
 router.post('/:id/devices', postDevices)
 
 // TODO: implement query parameters for this route (eventually use line 40 instead of line 41).
@@ -47,12 +43,16 @@ router.delete('/:id/devices/:deviceid', deleteUserDevices)
 
 router.put('/:id/devices/:deviceid', putUserDevices)
 
-// the three below methods were originally going to be handled by users service
+// the routes methods were originally going to be handled by users service
 // but they are now handled by the broker. Not deleting for sanity check, may need to rework.
 //router.get('/:id/devices/:id/connect', getConnectUserDevices)
 
 //router.put('/:id/devices/:id/disconnect', putDisconnectUserDevices)
 
 //router.get('/:id/devices/:id/adsb?start=<param>&end=<param> ', getAdsbUserDevices)
+
+//router.get('/:id/client/connect', getConnect)
+
+//router.put('/:id/client/disconnect', putDisconnect)
 
 module.exports = router
